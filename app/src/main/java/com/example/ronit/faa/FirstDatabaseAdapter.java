c7184f324ca9f07df67ca87e46be8c3507d80d34
Ronit Pradhan <ronit.pradhan@deerwalk.edu.np>
2016-11-03 20:19:11 +0545

    All activity pages made

--- app/src/main/java/com/example/ronit/faa/FirstDatabaseAdapter.java
+++ app/src/main/java/com/example/ronit/faa/FirstDatabaseAdapter.java
@@ -9,56 +9,52 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by RONIT on 11/2/2016.
  */
 
-public class FirstDatabaseAdapter{
+public class FirstDatabaseAdapter {
     FirstHelper helper;
-    public FirstDatabaseAdapter(Context context){
-        helper=new FirstHelper(context);
+
+    public FirstDatabaseAdapter(Context context) {
+        helper = new FirstHelper(context);
     }
 
-    public String getAllData() {
-        SQLiteDatabase db= helper.getWritableDatabase();
-        String[] columns={FirstHelper.UID, FirstHelper.NAME, FirstHelper.DETAIL};
-        Cursor cursor=db.query(FirstHelper.TABLE_NAME,columns,null,null,null,null,null);
+
+    public String getData(String search) {
+        SQLiteDatabase db = helper.getWritableDatabase();
+        String[] columns = {FirstHelper.NAME, FirstHelper.DETAIL};
+        Cursor cursor = db.query(FirstHelper.TABLE_NAME, columns, FirstHelper.NAME+"='"+search+"'", null, null, null, null);
         StringBuffer buffer=new StringBuffer();
-        while(cursor.moveToNext()) {
-            //int index1 =cursor.getColumnIndex(FirstHelper.UID);
-            //int cid=cursor.getInt(index1);
-            int cid=cursor.getInt(0);
-            String name=cursor.getString(1);
-            String detail=cursor.getString(2);
-            buffer.append(cid+" "+name+" "+detail+"\n");
+        while (cursor.moveToNext()) {
+            //int index1 = cursor.getColumnIndex(FirstHelper.NAME);
+            int index2 = cursor.getColumnIndex(FirstHelper.DETAIL);
+            //String name = cursor.getString(index1);
+            String detail = cursor.getString(index2);
+            buffer.append(detail);
 
         }
         return buffer.toString();
     }
 
     static class FirstHelper extends SQLiteOpenHelper {
-        private static final String DATABASE_NAME="first";
-        private static final String TABLE_NAME="info";
-        private static final int DATABASE_VERSION=11;
-        private static final String UID="_id";
-        private static final String NAME="name";
-        private static final String DETAIL="detail";
-        // private static final String DROP_TABLE="DROP TABLE IF EXISTS "+TABLE_NAME+";";
-        //private static final String CREATE_TABLE="CREATE TABLE "+TABLE_NAME+" ("+UID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+NAME+" VARCHAR(255), "+DETAIL+" LONGTEXT);";
+        private static final String DATABASE_NAME = "first";
+        private static final String TABLE_NAME = "info";
+        private static final int DATABASE_VERSION = 4;
+        private static final String UID = "_id";
+        private static final String NAME = "name";
+        private static final String DETAIL = "detail";
+        private static final String DROP_TABLE="DROP TABLE IF EXISTS "+TABLE_NAME+";";
+        private static final String CREATE_TABLE="CREATE TABLE "+TABLE_NAME+" ("+UID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+NAME+" VARCHAR(255), "+DETAIL+" LONGTEXT);";
         private Context context;
 
         public FirstHelper(Context context) {
             super(context, DATABASE_NAME, null, DATABASE_VERSION);
-            this.context=context;
+            this.context = context;
             Message.message(context, "constructor called");
+
         }
 
-        /**
-         * Called when the database is created for the first time. This is where the
-         * creation of tables and the initial population of the tables should happen.
-         *
-         * @param db The database.
-         */
-        //@Override
+        @Override
         public void onCreate(SQLiteDatabase db) {
 
-        /*try{
+       /* try{
             db.execSQL(CREATE_TABLE);
             Message.message(context, "onCreate called");
         }catch(SQLException e){
@@ -66,37 +62,15 @@ public class FirstDatabaseAdapter{
         }*/
         }
 
-        /**
-         * Called when the database needs to be upgraded. The implementation
-         * should use this method to drop tables, add tables, or do anything else it
-         * needs to upgrade to the new schema version.
-         * <p>
-         * <p>
-         * The SQLite ALTER TABLE documentation can be found
-         * <a href="http://sqlite.org/lang_altertable.html">here</a>. If you add new columns
-         * you can use ALTER TABLE to insert them into a live table. If you rename or remove columns
-         * you can use ALTER TABLE to rename the old table, then create the new table and then
-         * populate the new table with the contents of the old table.
-         * </p><p>
-         * This method executes within a transaction.  If an exception is thrown, all changes
-         * will automatically be rolled back.
-         * </p>
-         *
-         * @param db         The database.
-         * @param oldVersion The old database version.
-         * @param newVersion The new database version.
-         */
-        //@Override
+        @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         /*try {
             Message.message(context, "onUpgrade called");
             db.execSQL(DROP_TABLE);
             onCreate(db);
         }catch (SQLException e){
-
             Message.message(context,""+e);
         }*/
-
+        }
         }
     }
-}
