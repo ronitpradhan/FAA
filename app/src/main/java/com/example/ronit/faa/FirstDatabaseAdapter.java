2a965b85e64b1881ac94fb035b3aa0c025e76089
ronitpradhan <ronit.pradhan@deerwalk.edu.np>
2016-11-07 14:34:52 +0545

    Completed main feature

--- app/src/main/java/com/example/ronit/faa/FirstDatabaseAdapter.java
+++ app/src/main/java/com/example/ronit/faa/FirstDatabaseAdapter.java
@@ -2,9 +2,16 @@ package com.example.ronit.faa;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Random;
 
 /**
@@ -51,8 +58,9 @@ public class FirstDatabaseAdapter {
         }
         return buffer1.toString();
     }
-    static class FirstHelper extends SQLiteOpenHelper {
-        private static final String DATABASE_NAME = "first";
+     class FirstHelper extends SQLiteOpenHelper {
+        private static final String DB_PATH = "/data/data/com.example.ronit.faa/databases/";
+        private static final String DATABASE_NAME = "first.sqlite";
         private static final String TABLE_NAME = "info";
         private static final int DATABASE_VERSION = 4;
         private static final String UID = "_id";
@@ -60,6 +68,7 @@ public class FirstDatabaseAdapter {
         private static final String DETAIL = "detail";
         private static final String DROP_TABLE="DROP TABLE IF EXISTS "+TABLE_NAME+";";
         private static final String CREATE_TABLE="CREATE TABLE "+TABLE_NAME+" ("+UID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+NAME+" VARCHAR(255), "+DETAIL+" LONGTEXT);";
+        private SQLiteDatabase myDataBase;
         private Context context;
 
         public FirstHelper(Context context) {
@@ -68,7 +77,96 @@ public class FirstDatabaseAdapter {
             //Message.message(context, "constructor was called");
 
         }
+        public void createDataBase() throws IOException {
 
+            boolean dbExist = checkDataBase();
+
+            if (dbExist) {
+
+            } else {
+
+
+                this.getWritableDatabase();
+
+                try {
+
+                    copyDataBase();
+
+                } catch (IOException e) {
+
+                    throw new Error("Error copying database");
+
+                }
+            }
+
+        }
+
+        private boolean checkDataBase() {
+
+            SQLiteDatabase checkDB = null;
+
+            try {
+                String myPath = DB_PATH + DATABASE_NAME;
+                File file = new File(myPath);
+                if (file.exists() && !file.isDirectory())
+                    checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+
+            } catch (SQLiteException e) {
+
+
+            }
+
+            if (checkDB != null) {
+
+                checkDB.close();
+
+            }
+
+            return checkDB != null ? true : false;
+        }
+
+
+        private void copyDataBase() throws IOException {
+
+
+            InputStream myInput = context.getAssets().open(DATABASE_NAME);
+            String outFileName = DB_PATH + DATABASE_NAME;
+            System.out.println(outFileName+ " " );
+            OutputStream myOutput = new FileOutputStream(outFileName);
+
+
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = myInput.read(buffer)) > 0) {
+                myOutput.write(buffer, 0, length);
+            }
+
+
+            myOutput.flush();
+            myOutput.close();
+            myInput.close();
+
+        }
+
+        public void openDataBase() throws SQLException {
+
+
+            String myPath = DB_PATH + DATABASE_NAME;
+            File file = new File(myPath);
+            if (file.exists() && !file.isDirectory())
+                myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+
+        }
+
+        @Override
+        public synchronized void close() {
+
+            if (myDataBase != null)
+                myDataBase.close();
+
+            super.close();
+
+        }
         @Override
         public void onCreate(SQLiteDatabase db) {
 
--- app/src/main/java/com/example/ronit/faa/Home.java
+++ app/src/main/java/com/example/ronit/faa/Home.java
@@ -1,6 +1,7 @@
 package com.example.ronit.faa;
 
 import android.content.Intent;
+import android.database.SQLException;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -10,8 +11,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.IOException;
+
 public class Home extends AppCompatActivity{
-   FirstDatabaseAdapter firstHelper;
+    FirstDatabaseAdapter secondHelper;
+    FirstDatabaseAdapter.FirstHelper firstHelper;
     Button search_button;
     EditText edit_search;
 
@@ -20,13 +24,37 @@ public class Home extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        firstHelper=new FirstDatabaseAdapter(this);
-        String randText = firstHelper.getRandomData();
+        secondHelper = new FirstDatabaseAdapter(this);
+        firstHelper  = new FirstDatabaseAdapter(this).new FirstHelper(this);
+
+        try {
+
+            firstHelper.createDataBase();
+
+        } catch (IOException ioe) {
+
+            throw new Error("Unable to create database");
+
+        }
+
+        try {
+
+            firstHelper.openDataBase();
+
+        }catch(SQLException sqle){
+
+            throw sqle;
+
+        }
+
+        String randText = secondHelper.getRandomData();
         TextView tt1 = (TextView) findViewById(R.id.randomText);
         tt1.setText(randText);
 
         search_button= (Button) findViewById(R.id.search_button);
         edit_search=(EditText) findViewById(R.id.edit_search);
+
+
 
         search_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,7 +62,7 @@ public class Home extends AppCompatActivity{
             {
                 String search= edit_search.getText().toString();
                 Intent intent = new Intent(Home.this, Info.class);
-                String data= firstHelper.getData(search);
+                String data= secondHelper.getData(search);
                 //Message.message(Home.this,data);
                 intent.putExtra("keyHead", search);
                 intent.putExtra("key", data);
